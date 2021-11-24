4dbc3d007bf48fc3eeeda034c3356079cae3c1c5
overFlowBIN <bin1188@gmail.com>
2021-11-24 23:48:06 +0900

    test! add

--- 1124_HTTPserver/HTTPserver.js
+++ 1124_HTTPserver/HTTPserver.js
@@ -1,17 +1,21 @@
 const http = require('http')
 
+
+//! hello world를 응답해주는 서버 생성
+
+const server = http.createServer((requset, response) => {
+  response.write('hello world');
+  response.end();
+});
+
+// 또는
 // const server = http.createServer();
 // server.on('request', (req, res) => {
 //   res.write('hello world');
 //   res.end();
 // });
 
-const server = http.createServer((requset, response) => {
-  response.write('hello world');
-  response.end();
-})
-
-
-
 server.listen(3000);
+
+test!
 
