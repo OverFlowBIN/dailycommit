d820f6e289674728bbf0f04e55fe1c99eb782712
OverFlowBIN <bin11788@gmail.com>
2022-02-01 00:35:52 +0900

    0201_commit_1

--- algorithm/algorithm_basic/26_졸업 선물/26_졸업 선물.js
+++ algorithm/algorithm_basic/26_졸업 선물/26_졸업 선물.js
@@ -17,7 +17,7 @@
 
 
 // solve.1
-function solution(n, array) {
+function solution(N, arr) {
   // sort 해서 낮은 가격 부터 예산에서 빼기를 고려!
   let len = arr.length;
   let sort = [];
@@ -34,40 +34,66 @@ function solution(n, array) {
   // sort된  [ 2, 2 ], [ 4, 3 ], [ 4, 5 ], [ 6, 6 ], [ 10, 3 ] 
   // 배열을 이용하여 처음부터 하나씩 할인 된 가격을 더한값을 result 배열에 담는다.
   // 담은 배열중 length가 가장 긴 값을 구하자
-  let count = [];
+  
+  // 반복문으로 sort를 돌면서
+  // 순서대로 한번씩 할인을 그 비용을 원래 값에서 뺸다.
+  // 나머지도 비용이 작은 순서대로 N에서 뺸다. 뺄때마다 count++
+    // N은 계속 재사용해야 하므로 다른 값으로 복사해서 사용한다.
+
+  
+  let won = N;
+  let count = 0;
   let result;
   for(let i = 0; i < sort.length; i++) {
-    let result = 0;
+    result = 0;
+    won -= (sort[i][0]/2 + sort[i][1]);
+    count++;
     for(let j = 0; j < sort.length; j++) {
-      if(i === j) {
-        n -= (sort[i][0] / 2 + sort[i][1])
-        count++;
-      }
-      else {
-        n -= sort[j][0] + sort[j][1];
-        count++;
-      }
-      if(n < 0) {
+      if(won > 0) {
+        if(i !== j) {
+          won -= sort[j][0] + sort[j][1];
+          count++
+        }
         count--;
-        
       }
     }
-
-   result = Math.max(result, count);
-   console.log(result);
+    result = Math.max(result, count)
   }
-   
+
   return result;
-  
 }
 
 
-// 28 - 4
-// 24 - 7
-// 17 - 9
-// 8 - 8(10/2 + 배송비 5원) = 0원 총 4개
+
+// solve.2
+function solution(won, arr) {
+  let result = 0;
+  n = arr.length;
+  arr.sort((a, b) => (a[0] + a[1]) - (b[0] + b[1]))
+  // console.log(arr)...후...
+  
+  for(let i = 0; i < n; i++) {
+    let money = won - (arr[i][0] / 2 + arr[i][1]);
+    let count = 1;
+    for(let j = 0; j < n; j++) {
+      if(j !== i && arr[j][0] + arr[j][1] > money) break;
+      if(j !== i && arr[j][0] + arr[j][1] <= money) {
+        money -= arr[j][0] + arr[j][1];
+        count++;
+      }
+    }
+    result = Math.max(result, count)
+  }
+  return result;
+}
+
+
+
+
 
 // 입출력 예제
 let arr = [[6, 6], [2, 2], [4, 3], [4, 5], [10, 3]];
 let output = solution(28, arr);
 console.log(output);
+
+
